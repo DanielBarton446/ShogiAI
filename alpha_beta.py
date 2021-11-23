e6e67016c116bbdc0b01fcd8c5f9d5ece2338142
DanielBarton446 <Daniel.Barton446@gmail.com>
2021-11-23 01:21:47 -0600

    fix: count_material

--- alpha_beta.py
+++ alpha_beta.py
@@ -2,24 +2,42 @@ import python_shogi.shogi as ps
 import material_consts as mc
 
 def main():
-    game = ps.Board()
-    print(evaluator(game))
+    pass
+
+# Traditionally, player 1 is BLACK and player 2 is WHITE
+def legal_moves_for_current_player(board : ps.Board):
+    legal_moves = []
+    for mv in board.generate_legal_moves():
+        src = mv.from_square
+        piece = board.piece_at(src)
+        if piece.color == board.turn:
+            legal_moves.append(mv)
+    return legal_moves
+
 
 def count_material(board : ps.Board):
-    # This runs pretty slow -- around 0.0004 seconds per call
     value = 0
     for sq in ps.SQUARES:
-        mask = ps.BB_SQUARES[sq]
-        piece = str(board.piece_at(sq))
-        if piece is not None:
-            for piece_type in ps.PIECE_TYPES_WITHOUT_KING:
-                if mask & board.piece_bb[piece_type]:
-                    if piece.isupper():
-                        value += mc.ON_BOARD_VALUES.get(piece_type)
-                    elif piece.islower():
-                        value -= mc.ON_BOARD_VALUES.get(piece_type)
-    return value
+        piece = board.piece_at(sq)
+        if piece is not None and piece.piece_type is not ps.KING:
+            if piece.color == ps.BLACK:
+                value += mc.ON_BOARD_VALUES.get(piece.piece_type)
+            else:
+                value -= mc.ON_BOARD_VALUES.get(piece.piece_type)
+    
+    # board.pieces_in_hand gives a counter, Counter.elements() 
+    # expands the count of each piece.
+    # E.g. c = Counter({1:2}) is 2 pawns(per index defintion)
+    #  --- c.elements() evaluates to the list [1,1]
+    for e in board.pieces_in_hand[ps.BLACK].elements():
+        piece_type = ps.PIECE_TYPES_WITH_NONE[e] 
+        value += mc.IN_HAND_VALUES.get(piece_type)
 
+    for e in board.pieces_in_hand[ps.WHITE]:
+        piece_type = ps.PIECE_TYPES_WITH_NONE[e] 
+        value -= mc.IN_HAND_VALUES.get(piece_type)
+    
+    return value
 
 
 def evaluator(board : ps.Board):
