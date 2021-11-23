d64f20ef8c7ce2ab4b7c0bfb2adc4b1641053481
DanielBarton446 <Daniel.Barton446@gmail.com>
2021-11-23 01:21:18 -0600

    fixes count_material--adds promoted pieces in hand

--- material_consts.py
+++ material_consts.py
@@ -25,6 +25,15 @@ IN_HAND_VALUES = {
          Consts.PAWN  : 1.15 , Consts.LANCE: 4.80 , Consts.KNIGHT: 5.10,   
          Consts.SILVER: 7.20 , Consts.GOLD : 7.80 , 
          Consts.BISHOP: 11.10, Consts.ROOK : 12.70,
+
+         # Add Promoted Pieces to hand as well
+         # For some reason add_piece_into_hand allows promoted pieces to be 
+         # added, even though they can only be placed back as their unpromoted 
+         # versions.
+         Consts.PROM_PAWN  : 1.15 , Consts.PROM_LANCE: 4.80 , 
+         Consts.PROM_KNIGHT: 5.10 ,   
+         Consts.PROM_SILVER: 7.20 , 
+         Consts.PROM_BISHOP: 11.10, Consts.PROM_ROOK : 12.70,
          }
 
 
