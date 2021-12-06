518f46d13b0d7a55efd65e0a04437d2be08239af
Hamza122000 <Hamzabenkhalifa159@gmail.com>
2021-12-05 19:19:27 -0600

    Creating the AI_class to create a game, creatpython -u c:\Users\Hamza\Documents\shogi\ShogiAI\AI_class.py

--- alpha_beta.py
+++ alpha_beta.py
@@ -77,3 +77,52 @@ def my_alpha_beta(board : ps.Board,depth, maximizingplayer, alpha, beta):
             if beta <= alpha:
                 break
         return [best_move,min_value]
+
+
+
+def finale_alpha_beta(board : ps.Board,depth, maximizingplayer, alpha, beta, generator, evaluator):
+    if (depth == 0 or board.is_game_over()):
+        return [None, evaluator(board)]
+    elif(maximizingplayer):
+        max_value = float('-inf')
+        best_move = ""
+        moves = generator(board)
+        length = sum(1 for _ in moves)
+        if (length == 0):
+            print("the evaluator function provided did not provide any moves")
+            moves = board.generate_legal_moves()
+        else:
+            print("the evaluator function provided did provide some moves")
+            print()
+            moves = generator(board)
+        for move in moves:
+            current_move = move.usi()
+            board.push_usi(current_move)
+            evl = alpha_beta(board, depth-1, False,alpha,beta)
+            if (evl[1] > max_value):
+                best_move = current_move
+                max_value = evl[1]            
+            board.pop()
+            alpha = max(alpha,evl[1])
+            if beta <= alpha:
+                break
+
+        return [best_move, max_value]
+    else:
+        min_value = float('inf')
+        best_move = ""
+        moves = board.generate_legal_moves()
+        for move in moves:
+            current_move = move.usi()
+            board.push_usi(current_move)
+            evl = alpha_beta(board, depth-1,True,alpha,beta)
+            if (evl[1] < min_value):
+                min_value = evl[1]
+                best_move = current_move
+            board.pop()
+            beta = min(beta,evl[1])
+            if beta <= alpha:
+                break
+        return [best_move,min_value]
+
+
